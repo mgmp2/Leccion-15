6cec525896612f7e17c2caf1541f9f9000bab8e6
Miriam Mendoza <miriammp1997@gmail.com>
2017-02-23 17:17:00 -0500

    whithout validaciones

--- assets-form&onclick&function/js/main.js
+++ assets-form&onclick&function/js/main.js
@@ -3,13 +3,13 @@ var ap      = document.getElementById("lastname");
 var dni     = document.getElementById("dni");
 var dir     = document.getElementById("address");
 
-button.onclick = function(){
-
-  document.getElementById("text").innerHTML = "<strong>"+
-                                              "<ul>" +
-                                              "<li>" + "Nombre: " + nombre.value + "</li>" +
-                                              "<li>" + "Apellido: "+ ap.value + "</li>" +
-                                              "<li>" + "DNI: " + dni.value + "</li>" +
-                                              "<li>" + "Direccion: "+dir.value + "</li>"+
-                                              "</strong>";
+button.onclick = function(e){
+  e.preventDefault();
+    document.getElementById("text").innerHTML = "<strong>"+
+                                                  "<ul>" +
+                                                  "<li>" + "Nombre: " + nombre.value + "</li>" +
+                                                  "<li>" + "Apellido: "+ ap.value + "</li>" +
+                                                  "<li>" + "DNI: " + dni.value + "</li>" +
+                                                  "<li>" + "Direccion: "+dir.value + "</li>"+
+                                                  "</strong>";return false;
 }
