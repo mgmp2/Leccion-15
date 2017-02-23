be712ebde8f8e1c5868c2b299ee2e371742567ac
Miriam Mendoza <miriammp1997@gmail.com>
2017-02-23 12:02:28 -0500

    Finish usando function para mostrar datos

---
+++ assets-form&onclick&function/js/main.js
@@ -0,0 +1,15 @@
+var nombre  = document.getElementById("name");
+var ap      = document.getElementById("lastname");
+var dni     = document.getElementById("dni");
+var dir     = document.getElementById("address");
+
+button.onclick = function(){
+
+  document.getElementById("text").innerHTML = "<strong>"+
+                                              "<ul>" +
+                                              "<li>" + "Nombre: " + nombre.value + "</li>" +
+                                              "<li>" + "Apellido: "+ ap.value + "</li>" +
+                                              "<li>" + "DNI: " + dni.value + "</li>" +
+                                              "<li>" + "Direccion: "+dir.value + "</li>"+
+                                              "</strong>";
+}
