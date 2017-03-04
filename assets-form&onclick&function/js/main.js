034b00251b3e317a86def94e7ecb616a536ed74a
Miriam Gisella Mendoza Pilco <Miriam Gisella Mendoza Pilco>
2017-03-04 13:10:40 -0500

    Little modification

--- assets-form&onclick&function/js/main.js
+++ assets-form&onclick&function/js/main.js
@@ -5,11 +5,11 @@ var dni     = document.getElementById("dni");
 var dir     = document.getElementById("address");
 
 
-    document.getElementById("text").innerHTML = "<strong>"+
+    document.getElementById("text").innerHTML = "<strong><h2>"+ "Sus datos son:" + "</h2></strong>" +
                                                   "<ul>" +
                                                   "<li>" + "Nombre: " + nombre.value + "</li>" +
                                                   "<li>" + "Apellido: "+ ap.value + "</li>" +
                                                   "<li>" + "DNI: " + dni.value + "</li>" +
-                                                  "<li>" + "Direccion: "+dir.value + "</li>"+
-                                                  "</strong>";
+                                                  "<li>" + "Direccion: "+dir.value + "</li>";
+
 }
